278b9fc2d936d1af9698eebc2f510499083f2ba1
Adrian <kowal.adrian@interia.pl>
2020-05-26 22:25:08 +0200

    Initial commit

---
+++ src/main/java/spring/io/springiofileupload/SpringiofileuploadApplication.java
@@ -0,0 +1,24 @@
+package spring.io.springiofileupload;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class SpringiofileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringiofileuploadApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args -> {
+            storageService.deleteAll();
+            storageService.init();
+        });
+    }
+}
---
+++ src/main/java/spring/io/springiofileupload/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package spring.io.springiofileupload;
+
+public class StorageFileNotFoundException extends RuntimeException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
